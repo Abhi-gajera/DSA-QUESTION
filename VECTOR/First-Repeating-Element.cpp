a24f901e01f01eed67fcfcaf7c77b4f283fe76f2
Abhi patel <163433710+Abhi-gajera@users.noreply.github.com>
2024-04-28 05:00:57 -0400

    First Repeating Element

---
+++ VECTOR/First-Repeating-Element.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int repeting(vector<int> arr, int n)
+{
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        for (int j = i + 1; j < arr.size(); j++)
+        {
+            if (arr[i] == arr[j])
+            {
+                //  jo first number j print kravo hoy to return karvu  jo return ni jgyaye
+                // cout<<arr[i] kari to bdha repeting element print thay
+                return arr[i];
+            }
+        }
+    }
+
+    return -1;
+}
+
+int main()
+{
+
+    vector<int> arr{10, 5, 3, 4, 3, 5, 6};
+    int n = arr.size();
+
+    cout << repeting(arr, n);
+}
